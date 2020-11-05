790c9b02ba23e8621a8b5c54e70cf5e504ba2d22
Giang Nguyen <giangkawaii1999@gmail.com>
2020-11-04 22:07:00 -0500

    initial commit

---
+++ giangLab4/src/main/java/giang/nguyen/s301033256/GiangActivity.java
@@ -0,0 +1,14 @@
+package giang.nguyen.s301033256;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GiangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
