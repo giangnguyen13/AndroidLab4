e1a0f896cc46182b02c597c5726659f90f494b83
Giang Nguyen <giangkawaii1999@gmail.com>
2020-11-14 09:47:28 -0500

    WIP: layout sreens

---
+++ giangLab4/src/main/java/giang/nguyen/s301033256/NguyenAddPatientActivity.java
@@ -0,0 +1,13 @@
+package giang.nguyen.s301033256;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NguyenAddPatientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
