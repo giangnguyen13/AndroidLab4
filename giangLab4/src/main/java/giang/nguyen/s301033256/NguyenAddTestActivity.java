ad251dd8cddff7526f9f8be93083c4603ee01e92
Giang Nguyen <giangkawaii1999@gmail.com>
2020-11-15 16:11:03 -0500

    add calender + save test with patient

--- giangLab4/src/main/java/giang/nguyen/s301033256/NguyenAddTestActivity.java
+++ giangLab4/src/main/java/giang/nguyen/s301033256/NguyenAddTestActivity.java
@@ -1,12 +1,22 @@
 package giang.nguyen.s301033256;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+//import android.icu.util.Calendar;
+import java.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +29,11 @@ import giang.nguyen.s301033256.models.Test;
 public class NguyenAddTestActivity extends AppCompatActivity {
     private DataSource datasource;
     private RadioButton radioButton;
+    DatePickerDialog picker;
+    EditText datePicker;
+    EditText cholesterolInput;
+    EditText temperatureInput;
+    List<Patient> patients;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,39 +42,68 @@ public class NguyenAddTestActivity extends AppCompatActivity {
         datasource = new DataSource(this);
         try{
             datasource.open();
-            List<Test> values = datasource.getAllTests();
+            patients = datasource.getAllPatients();
         }catch (Exception e){
             Toast.makeText(getApplicationContext(),e.toString(), Toast.LENGTH_SHORT).show();
         }
+
+        ArrayAdapter<Patient> adapter = new ArrayAdapter<Patient>(this, R.layout.spinner_item, patients);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        final Spinner selectPatient = (Spinner)findViewById(R.id.giangSelectPatientDropdown);
+        selectPatient.setAdapter(adapter);
+
+        datePicker = (EditText) findViewById(R.id.giangTestDate);
+        cholesterolInput = (EditText) findViewById(R.id.giangCholesterolInput);
+        temperatureInput = (EditText) findViewById(R.id.giangTemperatureInput);
+
+        datePicker.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                datePicker.setInputType(InputType.TYPE_NULL);
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(NguyenAddTestActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                datePicker.setText(year + "/" + (monthOfYear + 1) + "/" + dayOfMonth);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+
 
         Button addTestBtn = (Button)findViewById(R.id.giangAddTestBtn);
         addTestBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Test newTest = null;
-                TextView cholesterolTv = (TextView)findViewById(R.id.giangCholesterolInput);
-                TextView temperatureTv = (TextView)findViewById(R.id.giangTemperatureInput);
-                TextView testDateTv = (TextView)findViewById(R.id.giangTestDate);
                 RadioGroup bloodPressureRadio = (RadioGroup)findViewById(R.id.giangBloodPressureRadio);
 
                 int selectedId = bloodPressureRadio.getCheckedRadioButtonId();
 
                 // find the radiobutton by returned id
-                radioButton = (RadioButton) findViewById(selectedId);
+                radioButton = (RadioButton)findViewById(selectedId);
 
                 Toast.makeText(NguyenAddTestActivity.this,
                         radioButton.getText(), Toast.LENGTH_SHORT).show();
 
-                String cholesterol = cholesterolTv.getText().toString();
-                String temperature = temperatureTv.getText().toString();
-                String testDate = testDateTv.getText().toString();
+                String cholesterol = cholesterolInput.getText().toString();
+                String temperature = temperatureInput.getText().toString();
+                String testDate = datePicker.getText().toString();
                 String bloodPressure = radioButton.getText().toString();
 
                 String info = String.format("%s %s %s %s",bloodPressure,cholesterol,temperature,testDate);
+                Patient patient = (Patient)selectPatient.getSelectedItem();
+
 
                 try{
-                    newTest = datasource.createTest(99,bloodPressure,cholesterol,temperature,testDate);
-                    Toast.makeText(getApplicationContext(),String.format("Create success, Test ID#%d",newTest.getId()), Toast.LENGTH_SHORT).show();
+                    newTest = datasource.createTest(patient.getId(),bloodPressure,cholesterol,temperature,testDate);
+                    Toast.makeText(getApplicationContext(),String.format("Create success, Test ID#%d, Patient ID#%d",newTest.getId(),patient.getId()), Toast.LENGTH_SHORT).show();
                 }catch (Exception e){
                     Toast.makeText(getApplicationContext(),e.toString(), Toast.LENGTH_SHORT).show();
                 }
@@ -77,7 +121,7 @@ public class NguyenAddTestActivity extends AppCompatActivity {
             List<Test> values = datasource.getAllTests();
             for (Test t : values){
                 String testInfo = String.format("Test#%d Patient #%d %s",t.getId(),t.getPatient_id(),t.getCholesterol());
-                Toast.makeText(getApplicationContext(),testInfo, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(),testInfo, Toast.LENGTH_SHORT).show();
             }
         }catch (Exception e){
             Toast.makeText(getApplicationContext(),e.toString(), Toast.LENGTH_SHORT).show();
