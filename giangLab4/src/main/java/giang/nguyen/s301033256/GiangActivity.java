5d2ed12ed9808d316f96ec4bbcf2c87c7fd7ab9f
Giang Nguyen <giangkawaii1999@gmail.com>
2020-11-16 22:08:20 -0500

    linear view working

--- giangLab4/src/main/java/giang/nguyen/s301033256/GiangActivity.java
+++ giangLab4/src/main/java/giang/nguyen/s301033256/GiangActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -24,6 +28,7 @@ public class GiangActivity extends AppCompatActivity {
     FloatingActionButton addTestFab;
     FloatingActionButton addPatientFab;
     boolean visible_flag = false;
+    List<Patient> values;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,9 +36,6 @@ public class GiangActivity extends AppCompatActivity {
 
         datasource = new DataSource(this);
         datasource.open();
-
-        List<Patient> values = datasource.getAllPatients();
-        Toast.makeText(getApplicationContext(),String.format("size is %d",values.size()), Toast.LENGTH_SHORT).show();
 
         fab = findViewById(R.id.giangFab);
         addTestFab = findViewById(R.id.giangAddTestFab);
@@ -65,11 +67,8 @@ public class GiangActivity extends AppCompatActivity {
         datasource.open();
         super.onResume();
         toggleFab(true);
-        List<Patient> values = datasource.getAllPatients();
-        //Toast.makeText(getApplicationContext(),String.format("size is %d",values.size()), Toast.LENGTH_SHORT).show();
-//        for (Patient p : values){
-//            Toast.makeText(getApplicationContext(),p.getFullName(), Toast.LENGTH_SHORT).show();
-//        }
+        values = datasource.getAllPatients();
+        loadPatients(values);
     }
 
     @Override
@@ -88,6 +87,46 @@ public class GiangActivity extends AppCompatActivity {
             addPatientFab.show();
             addTestFab.show();
             visible_flag = true;
+        }
+    }
+
+    private void loadPatients(List<Patient> patients){
+        LinearLayout linearLayout = findViewById(R.id.giangPatientListLinearLayout);
+        for(Patient p : patients){
+            final Button name = new Button(this);
+            name.setText(p.getFullName());
+            name.setTextColor(getResources().getColor(R.color.main_text_color));
+            name.setTextSize(getResources().getDimension(R.dimen.very_small_text));
+            TextView department = new TextView(this);
+            department.setText(p.getDepartment());
+            department.setTextColor(getResources().getColor(R.color.white));
+            department.setTextSize(getResources().getDimension(R.dimen.x_small_text));
+            department.setBackgroundColor(getResources().getColor(R.color.success));
+            department.setGravity(Gravity.CENTER);
+            View v = new View(this);
+            v.setLayoutParams(new LinearLayout.LayoutParams(
+                    TableRow.LayoutParams.MATCH_PARENT,
+                    5
+            ));
+            v.setBackgroundColor(getResources().getColor(R.color.black));
+
+            linearLayout.addView(name);
+            linearLayout.addView(department);
+            linearLayout.addView(v);
+
+//            name.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    try {
+//                        Patient storedPatient= datasource.getPatientByName(name.getText().toString());
+//                        storedPatient.getFirstName();
+//                        Toast.makeText(getApplicationContext(),name.getText().toString(), Toast.LENGTH_SHORT).show();
+//                    }catch (Exception e){
+//                        Toast.makeText(getApplicationContext(),e.toString(), Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                }
+//            });
         }
     }
 }
